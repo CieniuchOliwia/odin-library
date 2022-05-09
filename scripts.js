d4b75d72a5b8eaa240293efc37b9d98c23549025
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-05-09 19:45:55 +0200

    read or not property

--- scripts.js
+++ scripts.js
@@ -1,28 +1,30 @@
 const myLibrary = [{
-    author: 'Rowling',
-    title: 'Harry Potter',
-    numberOfPages: 232,
+    author: "J.K. Rowling",
+    title: "Harry Potter i Komnata Tajemnic",
+    numberOfPages: 363,
+    readOrNot: "read already",
     bookInfo: function () {
-        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages`)
+        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.readOrNot}`)
     },
 }, {
-    author: 'Cixin Liu',
-    title: 'Problem trzech ciał',
+    author: "Cixin Liu",
+    title: "Problem trzech ciał",
     numberOfPages: 452,
+    readOrNot: "not read yet",
     bookInfo: function () {
-     return (`${this.title} by ${this.author}, ${this.numberOfPages} pages`)
+        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.readOrNot}`)
     }
 }
 ];
 
 
-function Book(inputAuthor, inputTitle, inputNumberOfPages) {
+function Book(inputAuthor, inputTitle, inputNumberOfPages, inputReadOrNot) {
     this.author = inputAuthor
     this.title = inputTitle
     this.numberOfPages = inputNumberOfPages
-    //this.readOrNot = inputReadOrNot
+    this.readOrNot = inputReadOrNot
     this.bookInfo = function () {
-        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages`)
+        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.readOrNot}`)
     }
 }
 
@@ -30,8 +32,13 @@ function addBookToLibrary() {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let numberOfPages = document.getElementById("numberOfPages").value;
-    //let readOrNot=document.getElementsByName("readOrNot").value;
-    let book = new Book(author, title, numberOfPages);
+    let readOrNot="";
+    if(document.getElementById("read").checked) {
+        readOrNot="read already"
+    } else if (document.getElementById("unread").checked){
+        readOrNot="not read yet"
+    }
+    let book = new Book(author, title, numberOfPages,readOrNot);
     console.log(book);
     myLibrary.push(book);
     console.log(myLibrary);
@@ -41,7 +48,7 @@ function addBookToLibrary() {
 document.getElementById("addNewBook").addEventListener("click", addBookToLibrary)
 
 function displayAllBooks() {
-    document.querySelectorAll(".book").forEach(el=>el.remove());
+    document.querySelectorAll(".book").forEach(el => el.remove());
     for (i = 0; i <= myLibrary.length - 1; i++) {
         let placeForBooks = document.querySelector("#placeForBooks");
         let newDiv = document.createElement("div");
@@ -53,3 +60,6 @@ function displayAllBooks() {
 
 document.getElementById("showBooks").addEventListener("click", displayAllBooks)
 
+//dlaczego przy addEventListenerze nie muszę podawać nawiasów przy funkcji?
+//debugowanie krok po kroku
+//dlaczego wywala polskie znaki przy wyświetlaniu
