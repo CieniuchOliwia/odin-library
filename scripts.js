831152c5f1b61c2ee0f96988c45c87adc8c790f8
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-05-09 18:31:04 +0200

    displayAllBooks function

--- scripts.js
+++ scripts.js
@@ -1,24 +1,55 @@
-let myLibrary = [];
+const myLibrary = [{
+    author: 'Rowling',
+    title: 'Harry Potter',
+    numberOfPages: 232,
+    bookInfo: function () {
+        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages`)
+    },
+}, {
+    author: 'Cixin Liu',
+    title: 'Problem trzech ciał',
+    numberOfPages: 452,
+    bookInfo: function () {
+     return (`${this.title} by ${this.author}, ${this.numberOfPages} pages`)
+    }
+}
+];
 
 
-function Book(author, title, numberOfPages, readOrNot) {
-    this.author = author
-    this.title = title
-    this.numberOfPages = numberOfPages
-    this.readOrNot = readOrNot
+function Book(inputAuthor, inputTitle, inputNumberOfPages) {
+    this.author = inputAuthor
+    this.title = inputTitle
+    this.numberOfPages = inputNumberOfPages
+    //this.readOrNot = inputReadOrNot
     this.bookInfo = function () {
-        console.log(`${title.value} by ${author.value}, ${numberOfPages.value} pages, ${readOrNot.value}`)
+        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages`)
     }
 }
 
 function addBookToLibrary() {
-    console.log("ok");
-    let title=document.getElementById("title");
-    let author=document.getElementById("author");
-    let numberOfPages=document.getElementById("numberOfPages");
-    let readOrNot=document.getElementsByName("readOrNot");
-    let book= new Book(author,title,numberOfPages,readOrNot);
-    book.bookInfo();
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let numberOfPages = document.getElementById("numberOfPages").value;
+    //let readOrNot=document.getElementsByName("readOrNot").value;
+    let book = new Book(author, title, numberOfPages);
+    console.log(book);
+    myLibrary.push(book);
+    console.log(myLibrary);
+
 }
 
-document.getElementById("btn").addEventListener("click", addBookToLibrary)
+document.getElementById("addNewBook").addEventListener("click", addBookToLibrary)
+
+function displayAllBooks() {
+    document.querySelectorAll(".book").forEach(el=>el.remove());
+    for (i = 0; i <= myLibrary.length - 1; i++) {
+        let placeForBooks = document.querySelector("#placeForBooks");
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("book");
+        placeForBooks.appendChild(newDiv);
+        newDiv.textContent = myLibrary[i].bookInfo();
+    }
+}
+
+document.getElementById("showBooks").addEventListener("click", displayAllBooks)
+
