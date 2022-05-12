eb4eb79f2bec37a05739084e474954e7c0ac36f1
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-05-12 17:50:12 +0200

    refactoring

--- scripts.js
+++ scripts.js
@@ -2,32 +2,36 @@ const myLibrary = [{
     author: "J.K. Rowling",
     title: "Harry Potter i Komnata Tajemnic",
     numberOfPages: 363,
-    readOrNot: "read already",
-    bookInfo: function () {
-        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.readOrNot}`)
-    },
+    readOrNot: true,
+    bookInfo: bookInfofunction
 }, {
     author: "Cixin Liu",
     title: "Problem trzech ciał",
     numberOfPages: 452,
-    readOrNot: "not read yet",
-    bookInfo: function () {
-        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.readOrNot}`)
-    }
+    readOrNot: false,
+    bookInfo: bookInfofunction
 }
 ];
 
 
 function Book(inputAuthor, inputTitle, inputNumberOfPages, inputReadOrNot) {
-    this.author = inputAuthor
-    this.title = inputTitle
-    this.numberOfPages = inputNumberOfPages
-    this.readOrNot = inputReadOrNot
-    this.bookInfo = function () {
-        return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.readOrNot}`)
-    }
+    this.author = inputAuthor;
+    this.title = inputTitle;
+    this.numberOfPages = inputNumberOfPages;
+    this.readOrNot = inputReadOrNot;
+    this.bookInfo = bookInfofunction;
 }
 
+function bookInfofunction () {
+    return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${checkIfBookRead(this.readOrNot)}`)}
+
+function checkIfBookRead(read){
+    if (read) {
+        return "read already";
+    } else { 
+        return "not read yet";
+    }
+}
 
 function addBookToLibrary() {
     let title = document.getElementById("title").value;
@@ -35,9 +39,9 @@ function addBookToLibrary() {
     let numberOfPages = document.getElementById("numberOfPages").value;
     let readOrNot="";
     if(document.getElementById("read").checked) {
-        readOrNot="read already"
-    } else if (document.getElementById("unread").checked){
-        readOrNot="not read yet"
+        readOrNot=true;
+    } else {
+        readOrNot=false;
     }
     let book = new Book(author, title, numberOfPages,readOrNot);
     myLibrary.push(book);
@@ -45,15 +49,17 @@ function addBookToLibrary() {
     document.getElementById("author").value="";
     document.getElementById("numberOfPages").value="";
     document.getElementById("read").checked=false;
-    document.getElementById("unead").checked=false;
+    document.getElementById("unread").checked=false;
 }
 
-document.getElementById("save").addEventListener("click", addBookToLibrary) 
+document.getElementById("save").addEventListener("click", addBookToLibrary);
+
+
 
 function displayAllBooks() {
     document.querySelectorAll(".book").forEach(el => el.remove());
+    let placeForBooks = document.querySelector("#placeForBooks");
     for (i = 0; i <= myLibrary.length - 1; i++) {
-        let placeForBooks = document.querySelector("#placeForBooks");
         let newDiv = document.createElement("div");
         newDiv.classList.add("book");
         placeForBooks.appendChild(newDiv);
@@ -61,7 +67,7 @@ function displayAllBooks() {
     }
 }
 
-document.getElementById("showBooks").addEventListener("click", displayAllBooks)
+document.getElementById("showBooks").addEventListener("click", displayAllBooks);
 
 
 document.getElementById("formForNewBook").style.visibility="hidden";
