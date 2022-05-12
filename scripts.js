954311de2bbfe688c477ca59c9be491ebc32851a
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-05-12 20:54:51 +0200

    remove buttons

--- scripts.js
+++ scripts.js
@@ -22,13 +22,14 @@ function Book(inputAuthor, inputTitle, inputNumberOfPages, inputReadOrNot) {
     this.bookInfo = bookInfofunction;
 }
 
-function bookInfofunction () {
-    return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${checkIfBookRead(this.readOrNot)}`)}
+function bookInfofunction() {
+    return (`${this.title} by ${this.author}, ${this.numberOfPages} pages, ${checkIfBookRead(this.readOrNot)}`)
+}
 
-function checkIfBookRead(read){
+function checkIfBookRead(read) {
     if (read) {
         return "read already";
-    } else { 
+    } else {
         return "not read yet";
     }
 }
@@ -37,19 +38,19 @@ function addBookToLibrary() {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let numberOfPages = document.getElementById("numberOfPages").value;
-    let readOrNot="";
-    if(document.getElementById("read").checked) {
-        readOrNot=true;
+    let readOrNot = "";
+    if (document.getElementById("read").checked) {
+        readOrNot = true;
     } else {
-        readOrNot=false;
+        readOrNot = false;
     }
-    let book = new Book(author, title, numberOfPages,readOrNot);
+    let book = new Book(author, title, numberOfPages, readOrNot);
     myLibrary.push(book);
-    document.getElementById("title").value="";
-    document.getElementById("author").value="";
-    document.getElementById("numberOfPages").value="";
-    document.getElementById("read").checked=false;
-    document.getElementById("unread").checked=false;
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("numberOfPages").value = "";
+    document.getElementById("read").checked = false;
+    document.getElementById("unread").checked = false;
 }
 
 document.getElementById("save").addEventListener("click", addBookToLibrary);
@@ -64,17 +65,30 @@ function displayAllBooks() {
         newDiv.classList.add("book");
         placeForBooks.appendChild(newDiv);
         newDiv.textContent = myLibrary[i].bookInfo();
+        let removeBookButton = document.createElement("button");
+        removeBookButton.classList.add("book");
+        removeBookButton.id=myLibrary[i].title;
+        removeBookButton.textContent = "Remove";
+        newDiv.appendChild(removeBookButton);
+        removeBookButton.addEventListener("click", function (event) {
+            let index=myLibrary.findIndex((book)=>book.title==event.target.id);
+            console.log(index);
+            myLibrary.splice(index,1);
+            newDiv.remove();
+        })
+
     }
 }
 
 document.getElementById("showBooks").addEventListener("click", displayAllBooks);
 
 
-document.getElementById("formForNewBook").style.visibility="hidden";
+document.getElementById("formForNewBook").style.visibility = "hidden";
 
-document.getElementById("addNewBook").addEventListener("click", function(){
-    document.getElementById("formForNewBook").style.visibility="visible";
-}) 
+document.getElementById("addNewBook").addEventListener("click", function () {
+    document.getElementById("formForNewBook").style.visibility = "visible";
+})
+
 
 
 
